004cb7c6ee0b1f1049db6d75e1f8322a12f96014
chendrenluc <chendren@luc.edu>
2025-12-02 09:01:01 -0600

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -20,116 +20,116 @@ import java.util.List;
  */
 
 public class Graph {
-  int numVertices;                  // vertices in graph
-  LinkedList<Integer>[] adjListArr; // Adjacency list
-  List<Integer> vertexValues;       // vertex values
+    int numVertices;                  // vertices in graph
+    LinkedList<Integer>[] adjListArr; // Adjacency list
+    List<Integer> vertexValues;       // vertex values
 
-  // Constructor 
-  public Graph(int numV) {
-    numVertices = numV;
-    adjListArr = new LinkedList[numVertices];
-    vertexValues = new ArrayList<>(numVertices);
+    // Constructor
+    public Graph(int numV) {
+        numVertices = numV;
+        adjListArr = new LinkedList[numVertices];
+        vertexValues = new ArrayList<>(numVertices);
 
-    for (int i = 0; i < numVertices; i++) {
-      adjListArr[i] = new LinkedList<>();
-      vertexValues.add(0);
-    }
-  }
-
-  /*
-   * method setValue
-   * 
-   * Sets a vertex's (node's) value.
-   */ 
-  
-  public void setValue(int vertexIndex, int value) {
-    if (vertexIndex >= 0 && vertexIndex < numVertices) {
-      vertexValues.set(vertexIndex, value);
-    } else {
-      throw new IllegalArgumentException(
-             "Invalid vertex index: " + vertexIndex);
-    }
-  }
-
-
-  public void addEdge(int src, int dest) {
-    adjListArr[src].add(dest);
-  }
-
-  /*
-   * method printGraph
-   * 
-   * Prints the graph as an adjacency matrix
-   */ 
-  
-  public void printGraph() {
-    System.out.println(
-         "\nAdjacency Matrix Representation:\n");
-    int[][] matrix = new int[numVertices][numVertices];
-
-    for (int i = 0; i < numVertices; i++) {
-      for (Integer dest : adjListArr[i]) {
-        matrix[i][dest] = 1;
-      }
+        for (int i = 0; i < numVertices; i++) {
+            adjListArr[i] = new LinkedList<>();
+            vertexValues.add(0);
+        }
     }
 
-    System.out.print("  ");
-    for (int i = 0; i < numVertices; i++) {
-      System.out.print(i + " ");
-    }
-    System.out.println();
+    /*
+     * method setValue
+     *
+     * Sets a vertex's (node's) value.
+     */
 
-    for (int i = 0; i < numVertices; i++) {
-      System.out.print(i + " ");
-      for (int j = 0; j < numVertices; j++) {
-        if (matrix[i][j] == 1) {
-          System.out.print("| ");
+    public void setValue(int vertexIndex, int value) {
+        if (vertexIndex >= 0 && vertexIndex < numVertices) {
+            vertexValues.set(vertexIndex, value);
         } else {
-          System.out.print(". ");
+            throw new IllegalArgumentException(
+                    "Invalid vertex index: " + vertexIndex);
         }
-      }
-      System.out.println();
     }
-  }
 
 
-  /**
-   * method findRoot
-   *
-   * This method returns the value of the root vertex, where root is defined in
-   * this case as a node that has no incoming edges. If no root vertex is found
-   * and/or more than one root vertex, then return -1.
-   * 
-   */
-  
-  public int findRoot() {
-
-    // ADD YOUR CODE HERE - DO NOT FORGET TO ADD YOUR NAME/SECTION AT TOP OF FILE
-
-    //Make an array for the number of vertices
-    int[] intDegree = new int[numVertices];
-    //Count the incoming edges
-    for(int src = 0; src < numVertices; src++) {
-      for(int dest : adjListArr[src]) {
-        intDegree[dest]++;
-      }
+    public void addEdge(int src, int dest) {
+        adjListArr[src].add(dest);
     }
-    //Initialize an int for the root index
-    //Return this int if invalid
-    int rootIndex = -1;
-    for(int i = 0; i < numVertices; i++) {
-      if(intDegree[i] == 0) {
-        if(rootIndex != -1) {
-          return -1;
+
+    /*
+     * method printGraph
+     *
+     * Prints the graph as an adjacency matrix
+     */
+
+    public void printGraph() {
+        System.out.println(
+                "\nAdjacency Matrix Representation:\n");
+        int[][] matrix = new int[numVertices][numVertices];
+
+        for (int i = 0; i < numVertices; i++) {
+            for (Integer dest : adjListArr[i]) {
+                matrix[i][dest] = 1;
+            }
         }
-        rootIndex = i;
-      }
+
+        System.out.print("  ");
+        for (int i = 0; i < numVertices; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < numVertices; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < numVertices; j++) {
+                if (matrix[i][j] == 1) {
+                    System.out.print("| ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
     }
-    //If no root is found, also return -1
-    if(rootIndex == -1) {
-      return -1;
+
+
+    /**
+     * method findRoot
+     *
+     * This method returns the value of the root vertex, where root is defined in
+     * this case as a node that has no incoming edges. If no root vertex is found
+     * and/or more than one root vertex, then return -1.
+     *
+     */
+
+    public int findRoot() {
+
+        // ADD YOUR CODE HERE - DO NOT FORGET TO ADD YOUR NAME/SECTION AT TOP OF FILE
+
+        //Make an array for the number of vertices
+        int[] indegree = new int[numVertices];
+        //Count the incoming edges
+        for(int src = 0; src < numVertices; src++) {
+            for(int dest : adjListArr[src]) {
+                indegree[dest]++;
+            }
+        }
+        //Initialize an int for the root index
+        //Return this int if invalid
+        int rootIndex = -1;
+        for(int i = 0; i < numVertices; i++) {
+            if(indegree[i] == 0) {
+                if(rootIndex != -1) {
+                    return -1;
+                }
+                rootIndex = i;
+            }
+        }
+        //If no root is found, also return -1
+        if(rootIndex == -1) {
+            return -1;
+        }
+        //Otherwise return the value stored at that vertex
+        return vertexValues.get(rootIndex);
     }
-    //Otherwise return the value stored at that vertex
-    return vertexValues.get(rootIndex);
-  } 
 }
